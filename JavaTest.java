204f3c71f6591e3de5aa72ad1ba4d0199b52f23e
linas <linas.einingis@labochema.lt>
2020-09-30 23:48:54 +0300

    Added sorting, accepted date range and padding for years with digit count less than 4

--- JavaTest.java
+++ JavaTest.java
@@ -4,13 +4,27 @@ import java.util.*;
 
 class JavaTest {
     public static void main(String[] args) {
-        printBonusDatesBetween(1990, 2020);
+        try {
+            printBonusDatesBetween(2010, 2015);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
     }
 
-    static void printBonusDatesBetween(int fromYear, int toYear) {
+    static void printBonusDatesBetween(int fromYear, int toYear) throws Exception {
         // Generate a set of valid years (from incl, to excl)
+        
+        if (fromYear < 0 || toYear > 9999) {
+            throw new Exception("Invalid years provided. Valid values are from 0 to 9999");
+        }
+        
+        // Use set for uniqueness
         Set<Integer> years = getNumbersBetween(fromYear, toYear - 1);
-        Set<String> validYears = new HashSet<String>();
+
+        // User Array List for sorting
+        ArrayList<String> validYears = new ArrayList<String>();
+
         /**
          * The only possibility for a date to be equal to its reversed value is to have
          * the month and day to equal the exact reverse of the year.
@@ -21,7 +35,11 @@ class JavaTest {
 
         for (Integer year : years) {
             String yearStr = String.valueOf(year);
-            String reverseYear = reverseIntToStr(year);
+            String reverseYearUnPadded = reverseIntToStr(year);
+            
+            // Make sure to pad zeroes if string length is not 4
+            String reverseYear = String.format("%1$" + 4 + "s", reverseYearUnPadded).replace(' ', '0');
+
             String month = reverseYear.substring(0, 2);
             String day = reverseYear.substring(2, 4);
             
@@ -31,7 +49,11 @@ class JavaTest {
             }
         }
 
-        System.out.println(validYears);
+        Collections.sort(validYears, new SortByStringDate());
+        
+        for (String validYear : validYears) {
+            System.out.println(validYear);
+        }
     }
 
     static Set<Integer> getNumbersBetween(int from, int to) {
@@ -67,5 +89,17 @@ class JavaTest {
         }
 
         return true;
+    }
+}
+
+class SortByStringDate implements Comparator<String> 
+{
+    public int compare(String a, String b) 
+    {
+        return Integer.parseInt(replaceDashes(a)) > Integer.parseInt(replaceDashes(b)) ? 1 : -1;
+    }
+
+    private String replaceDashes(String str) {
+        return str.replaceAll("-", "");
     }
 }
